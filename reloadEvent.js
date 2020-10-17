5df3fb727371b3a3d5f22b1daf5c06c8b86b5b97
A-Zalt <51456251+A-Zalt@users.noreply.github.com>
2020-10-17 10:50:37 +0300

    add info comment

--- reloadEvent.js
+++ reloadEvent.js
@@ -1,2 +1,20 @@
+/*
+######################################
+#              #INFO#                #
+#              ######                #
+#                                    #
+#       Filename: reloadEvent.js     #
+#    Description: Exports one        #
+#    instance of EventEmitter.       #
+#      What this script does:        #
+#     exports an EventEmitter        #
+#                                    #
+#                                    #
+#                                    #
+#                                    #
+#                                    #
+#                                    #
+######################################
+*/
 const event = require("events")
 module.exports = new event.EventEmitter()
