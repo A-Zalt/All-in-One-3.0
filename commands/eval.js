36d8928eed054331171fc04f1654c3fbaa4aef40
A-Zalt <51456251+A-Zalt@users.noreply.github.com>
2020-10-19 23:33:30 +0300

    make eval actually work with codeblocks

--- commands/eval.js
+++ commands/eval.js
@@ -6,7 +6,8 @@ module.exports = {
     admin: true,
     execute: async (message, _, bot, args) => {
         try {
-            message.channel.send(require("util").inspect(await eval(args.join(' ').replace("```", "")), true, 0).substr(0, 2000).replace(new RegExp(lodash.escapeRegExp(bot.token)), "[token omitted]").replace(new RegExp(lodash.escapeRegExp(`[${bot.token.split("").join(", ")}]`)), "[token omitted]"))
+            console.log(args.join(' ').startsWith("```js") && args.join(' ').endsWith("```") ? args.join(" ").substr(4, args.join(" ").length-8): args.join(" "))
+            message.channel.send(require("util").inspect(await eval(args.join(' ').startsWith("```js") && args.join(' ').endsWith("```") ? args.join(" ").substr(5, args.join(" ").length-8): args.join(" ")), true, 0).substr(0, 2000).replace(new RegExp(lodash.escapeRegExp(bot.token)), "[token omitted]").replace(new RegExp(lodash.escapeRegExp(`[${bot.token.split("").join(", ")}]`)), "[token omitted]"))
         } catch(err) {
             message.channel.send(err.stack, {code: "js"})
         }
